bcd40897742ad3e287e1bd231b914778d7786afd
gabrielhastec <gabrielhastec.dev@gmail.com>
2026-01-31 17:04:10 -0300

    Chore(infra): Adiciona a infraestrutura de autenticação da aplicação.

---
+++ app/infrastructure/auth/jwt_service.py
@@ -0,0 +1,16 @@
+
+from datetime import datetime, timedelta
+from jose import JWTError, jwt
+from passlib.context import CryptContext
+
+SECRET_KEY = "your-secret-key"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
